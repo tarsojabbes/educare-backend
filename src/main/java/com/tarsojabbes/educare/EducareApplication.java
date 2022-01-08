586ee0edc47f30bd6a6d05dbedb7f098a5be5057
Tarso Jabbes <tarsojabbes@gmail.com>
2022-01-07 22:02:52 -0300

    Initial commit

---
+++ src/main/java/com/tarsojabbes/educare/EducareApplication.java
@@ -0,0 +1,13 @@
+package com.tarsojabbes.educare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducareApplication.class, args);
+	}
+
+}
