767420afb89f78164eb31e20062d35ec7cf21979
Tarso Jabbes <tarsojabbes@gmail.com>
2022-01-11 12:38:52 -0300

    Implementa rotas de CRUD para Questao

--- src/main/java/com/tarsojabbes/educare/resources/QuestaoResource.java
+++ src/main/java/com/tarsojabbes/educare/resources/QuestaoResource.java
@@ -1,9 +1,15 @@
 package com.tarsojabbes.educare.resources;
 
+import com.tarsojabbes.educare.domains.Questao;
 import com.tarsojabbes.educare.services.QuestaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/questoes")
@@ -11,4 +17,35 @@ public class QuestaoResource {
 
     @Autowired
     private QuestaoService questaoService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Questao>> findAll(){
+        List<Questao> questoes = questaoService.findAll();
+        return ResponseEntity.ok().body(questoes);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public ResponseEntity<Optional<Questao>> findById(@PathVariable Integer id){
+        Optional<Questao> questao = questaoService.findById(id);
+        return ResponseEntity.ok().body(questao);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Questao questao){
+        questaoService.insert(questao);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(questao.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        questaoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody Questao questao, @PathVariable Integer id) {
+        questaoService.update(questao, id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/tarsojabbes/educare/services/QuestaoService.java
+++ src/main/java/com/tarsojabbes/educare/services/QuestaoService.java
@@ -1,12 +1,47 @@
 package com.tarsojabbes.educare.services;
 
+import com.tarsojabbes.educare.domains.Questao;
 import com.tarsojabbes.educare.repositories.QuestaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestaoService {
 
     @Autowired
     private QuestaoRepository questaoRepository;
+
+    public List<Questao> findAll(){
+        List<Questao> questoes = questaoRepository.findAll();
+        return questoes;
+    }
+
+    public Optional<Questao> findById(Integer id){
+        Optional<Questao> questao = questaoRepository.findById(id);
+        return questao;
+    }
+
+    public void insert(Questao questao) {
+        questao.setId(null);
+        questaoRepository.save(questao);
+    }
+
+    public void delete(Integer id) {
+        findById(id);
+        try {
+            questaoRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Não foi possível excluir essa questão");
+        }
+    }
+
+    public void update(Questao questao, Integer id) {
+        findById(id);
+        questaoRepository.save(questao);
+    }
+
 }
